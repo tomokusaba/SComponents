7f42ccd3ac159928c46aea79a80b07b022a3fbcd
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2024-03-27 06:18:43 +0900

    applinsghts

--- SComponents/Program.cs
+++ SComponents/Program.cs
@@ -12,6 +12,10 @@ builder.Services.AddSmartComponents()
     .WithInferenceBackend<OpenAIInferenceBackend>();
 builder.Services.AddHttpClient();
 builder.Services.AddFluentUIComponents();
+builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
+{
+    ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+});
 
 var app = builder.Build();
 
